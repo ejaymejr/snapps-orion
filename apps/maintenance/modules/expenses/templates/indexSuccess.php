ad115b14d60beb157e517312520a606b4de55b2f
ejaymejr <ejaymejr@gmail.com>
2016-03-01 12:19:50 +0800

    added icons to maintenance index

---
+++ apps/maintenance/modules/expenses/templates/indexSuccess.php
@@ -0,0 +1,9 @@
+<div class="contentBox">
+	<table class="table bordered condensed">
+		<tr>
+			<td><?php echo image_tag("hr/maintenance.gif")?></td>
+			<td><?php echo image_tag("hr/maintenance.gif")?></td>
+			<td><?php echo image_tag("hr/maintenance.gif")?></td>
+		</tr>
+	</table>
+</div>
