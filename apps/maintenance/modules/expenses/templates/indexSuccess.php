9116d3430bc89a3360ce18fd7d3137eade6982c7
ejaymejr <ejaymejr@gmail.com>
2016-03-02 09:51:08 +0800

    centered images

--- apps/maintenance/modules/expenses/templates/indexSuccess.php
+++ apps/maintenance/modules/expenses/templates/indexSuccess.php
@@ -1,9 +1,9 @@
 <div class="contentBox">
 	<table class="table bordered condensed">
 		<tr>
-			<td><?php echo image_tag("hr/maintenance.gif")?></td>
-			<td><?php echo image_tag("hr/maintenance.gif")?></td>
-			<td><?php echo image_tag("hr/maintenance.gif")?></td>
+			<td class="text-center"><?php echo image_tag("hr/maintenance.gif")?></td>
+			<td class="text-center"><?php echo image_tag("hr/maintenance.gif")?></td>
+			<td class="text-center"><?php echo image_tag("hr/maintenance.gif")?></td>
 		</tr>
 	</table>
 </div>
